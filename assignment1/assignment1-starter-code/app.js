c2960ce61c50fc5602cd58b734990c3022925572
Carla Soares <Carla Soares>
2016-11-11 16:33:43 -0200

    First assigment

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -2,48 +2,73 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope']
 function LunchCheckController($scope){
 
+  $scope.lunchMenu = '';
+  $scope.statusLunch = false;
 
- $scope.checkIfToMuch = function () {
-
-   $scope.sayMessage = function () {
-
-     var stringLunchMenu = $scope.lunchMenu;
-     var totalLunchMenu = stringLunchMenu.split(',');
-     var totalItens = 0;
-
-      for (var i = 0; i < totalLunchMenu.length; i++)
-      {
-        
-        if (totalLunchMenu[i] != "")
-        {
-          totalItens++;
-          console.log(totalItens);
-        }
+  $scope.limparMessage = function(){
+      if(  $scope.statusLunch === true) {
+        $scope.statusLunch = false;    
+          $scope.sayMessage = function () {
+            return "";
+         }
       }
+  }
 
-     if (totalItens === 0)
-     {
-       return  "Check If Too Much";
-     }
+  $scope.checkIfToMuch = function() {
 
-    if (totalItens <= 3)
-     {
-       return  "Enjoy!";
-     }
+    $scope.statusLunch = true;
 
-   if (totalItens > 3)
-     {
-       return "Too much!";
-     }
+    $scope.sayMessage = function () {
+
+        var stringLunchMenu = $scope.lunchMenu;
+
+        if (stringLunchMenu !== undefined){
+
+          var totalLunchMenu = stringLunchMenu.split(',');
+          var totalItens = 0;
+
+           for (var i = 0; i < totalLunchMenu.length; i++)
+           {
+
+             if (totalLunchMenu[i].trim() !== "")
+             {
+               totalItens++;
+
+             }
+           }
+
+          if (totalItens === 0)
+          {
+           $scope.corMessage = "corRed";
+
+            return  "Please enter data first";
+          }
+
+         if (totalItens <= 3)
+          {
+           $scope.corMessage = "corGreen";
+
+            return  "Enjoy!";
+          }
+
+        if (totalItens > 3)
+          {
+           $scope.corMessage = "corGreen";
+
+            return "Too much!";
+          }
+        }else{
+             $scope.corMessage = "corRed";
+             return  "Please enter data first";
+        }
+
+     };
   };
-
-};
 }
 
 })();
