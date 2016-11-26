497624aca538329cd618ea780b248ceccfdd11ff
Carla Soares <Carla Soares>
2016-11-26 12:31:07 -0200

    teste 6

--- assignment3/module3/app.js
+++ assignment3/module3/app.js
@@ -4,7 +4,7 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.constant('ApiBasePath', "https://csoares1.github.io/courseraTesteAngular/assignment3/module3")
 .directive('foundItems', NarrowItDownDirective);
 
 function NarrowItDownDirective() {
